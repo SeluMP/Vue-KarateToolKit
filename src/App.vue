f82bbf15119a65d25dca3918845267c89296cd8f
alejandro4cs4 <alejandro4cs4@gmail.com>
2021-12-08 14:13:51 +0100

    Created home page structure

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,25 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      elevation="12"
+      color="#114B5F"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <div>Logo</div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <div>
+        <v-btn class="mr-4 nav-link" color="#F45B69">
+          Login Token
+        </v-btn>
+        <v-btn class="mr-4 nav-link" color="#F45B69">
+          Login Junta/Ayuntamiento
+        </v-btn>
+      </div>
     </v-app-bar>
 
-    <v-main>
+    <v-main class="mt-6">
       <mycomponent/>
     </v-main>
   </v-app>
@@ -58,3 +41,13 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+body {
+  height: 100vh;
+}
+
+.nav-link {
+  text-transform: unset;
+}
+</style>
--- src/components/mycomponent.vue
+++ src/components/mycomponent.vue
@@ -1,3 +1,25 @@
 <template>
-    <v-btn> Pulse si santa es trolo </v-btn>
+    <v-container>
+        <v-row style="height: 100%;">
+            <v-col class="left-side" cols="8"></v-col>
+            <v-col class="right-side" cols="4"></v-col>
+        </v-row>
+    </v-container>
 </template>
+
+<style scoped>
+.container {
+    width: 100%;
+    height: 100%;
+}
+
+.left-side {
+    height: 100%;
+    background-color: red;
+}
+
+.right-side {
+    height: 100%;
+    background-color: green;
+}
+</style>
