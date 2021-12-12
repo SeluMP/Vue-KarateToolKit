7eaa588334f6be9eb13b2d01182265afd86a1963
alejandro4cs4 <alejandro4cs4@gmail.com>
2021-12-12 12:17:54 +0100

    Changed ComentarioCard img

--- src/components/ComentarioCard.vue
+++ src/components/ComentarioCard.vue
@@ -3,7 +3,9 @@
     class="px-4 py-4 d-flex align-center card"
     elevation="6"
   >
-    <v-img class="mr-5" src="@/assets/imagenes/account.png" max-width="80px"></v-img>
+    <div class="user-container">
+      <v-img src="@/assets/imagenes/person_black_24dp.svg" max-width="80px"></v-img>
+    </div>
     <div class="content" style="text-align: left;">
       <p style="font-weight: bold; margin-top: 0; margin-bottom: 5px;">Maria</p>
       <p style="height: 100%; margin-bottom: 0;">Mi hijo se lo pasó muy bien</p>
@@ -24,6 +26,20 @@ body #app .card {
   position: relative;
   margin-bottom: 1.4em;
   display: flex;
+}
+
+body #app .user-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 68px;
+  height: 68px;
+  padding: .5em;
+  margin-right: 20px;
+  background-color: #f8f8f8;
+  border-radius: 50%;
+  border: 2px solid black;
 }
 
 @media only screen and (max-width: 850px) {
